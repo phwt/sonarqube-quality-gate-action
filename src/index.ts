194bdfdc020c64da487d49050634eac50c983000
phwt <28344318+phwt@users.noreply.github.com>
2022-10-05 22:25:41 +0700

    refactor: allow other events

--- src/index.ts
+++ src/index.ts
@@ -14,9 +14,8 @@ import { formatMetricKey, getStatusEmoji, getComparatorSymbol } from "./utils";
     // TODO: Output result
 
     const commentDisabled = core.getInput("disable-pr-comment") == "true";
-    const isPR = github.context.eventName == "pull_request";
 
-    if (isPR && !commentDisabled) {
+    if (!commentDisabled) {
       const resultTable = result.projectStatus.conditions
         .map((condition) => {
           const tableValues = [
