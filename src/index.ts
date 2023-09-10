81f412252f28085d15efaaf482a039ccceb13898
Steffen Bach <steffen.bach@exxeta.com>
2023-09-10 22:48:58 +0200

    Added rudimentary branching support

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import * as core from "@actions/core";
 import * as github from "@actions/github";
 import { buildReport } from "./modules/report";
 import { ActionInputs } from "./modules/models";
-import { fetchQualityGate } from "./modules/sonarqube-api";
+import { fetchQualityGate, fetchQualityGateWithBranch } from "./modules/sonarqube-api";
 import { trimTrailingSlash } from "./modules/utils";
 import { findComment } from "./modules/find-comment/main";
 
@@ -19,12 +19,21 @@ import { findComment } from "./modules/find-comment/main";
       githubToken: core.getInput("github-token"),
     };
 
-    const result = await fetchQualityGate(
-      inputs.hostURL,
-      inputs.projectKey,
-      inputs.branch,
-      inputs.token
-    );
+    if (!inputs.branch) {
+      const result = await fetchQualityGate(
+        inputs.hostURL,
+        inputs.projectKey,
+        inputs.token
+      );
+    } else {
+      const result = await fetchQualityGateWithBranch(
+        inputs.hostURL,
+        inputs.projectKey,
+        inputs.branch,
+        inputs.token
+      );
+    }
+
 
     core.setOutput("project-status", result.projectStatus.status);
     core.setOutput("quality-gate-result", JSON.stringify(result));
--- src/modules/sonarqube-api.ts
+++ src/modules/sonarqube-api.ts
@@ -1,10 +1,10 @@
 import axios from "axios";
 import { QualityGate } from "./models";
 
-export const fetchQualityGate = async (
+export const fetchQualityGateWithBranch = async (
   url: string,
   projectKey: string,
-  branch: string | undefined,
+  branch: string,
   token: string
 ): Promise<QualityGate> => {
   const response = await axios.get<QualityGate>(
@@ -13,6 +13,27 @@ export const fetchQualityGate = async (
       params: {
         projectKey,
         branch,
+      },
+      auth: {
+        username: token,
+        password: "",
+      },
+    }
+  );
+
+  return response.data;
+};
+
+export const fetchQualityGate = async (
+  url: string,
+  projectKey: string,
+  token: string
+): Promise<QualityGate> => {
+  const response = await axios.get<QualityGate>(
+    `${url}/api/qualitygates/project_status`,
+    {
+      params: {
+        projectKey,
       },
       auth: {
         username: token,
