b513e99a98b9acb8b740286197d59e65dca7143c
phwt <28344318+phwt@users.noreply.github.com>
2025-09-17 23:15:00 +0700

    Feature/step summary (#42)
    * Add new input for conditionally commenting the result in the step summary
    
    * style/refactor: formatting and remove redundant log
    
    * chore: default `disable-step-summary` to `false`
    
    * docs: update inputs description
    
    ---------
    
    Co-authored-by: Jolu Izquierdo <22649229+joluizquierdo@users.noreply.github.com>

---
+++ src/modules/summary.ts
@@ -0,0 +1,39 @@
+import { ActionInputs, QualityGate } from "./models";
+import { buildReport } from "./report";
+import * as fs from "fs";
+
+export const addSummary = async ({
+  inputs,
+  result,
+  github,
+}: {
+  inputs: ActionInputs;
+  result: QualityGate;
+  github: typeof import("@actions/github");
+}) => {
+  const { context } = github;
+
+  const reportBody = buildReport(
+    result,
+    inputs.hostURL,
+    inputs.projectKey,
+    context,
+    inputs.branch,
+    inputs.pullRequest
+  );
+
+  console.log("Adding report to the step summary...");
+  const summaryFilePath = process.env.GITHUB_STEP_SUMMARY;
+
+  if (!summaryFilePath) throw new Error("GITHUB_STEP_SUMMARY is not defined.");
+
+  try {
+    fs.appendFileSync(summaryFilePath, reportBody + "\n");
+  } catch (error) {
+    if (error instanceof Error) {
+      throw new Error(
+        `Failed to write to GITHUB_STEP_SUMMARY: ${error.message}`
+      );
+    }
+  }
+};
